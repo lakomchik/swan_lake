eef014929e1b08f051b49717429a97af19e91e55
Ivan Apanasevich <ivanapanasevich@mail.ru>
2023-05-20 11:16:40 +0300

    Modified preprocessor

--- src/preprocess/preprocess.py
+++ src/preprocess/preprocess.py
@@ -1,3 +1,9 @@
+import warnings
+import cv2
+import pandas as pd
+from tqdm.autonotebook import tqdm
+from albumentations.pytorch import ToTensorV2
+import albumentations as A
 import numpy as np
 import os
 import random
@@ -5,24 +11,28 @@ import torch
 import PIL
 from PIL import Image, __version__
 PIL.PILLOW_VERSION = __version__
-import albumentations as A
-from albumentations.pytorch import ToTensorV2
-from tqdm.autonotebook import tqdm
-import pandas as pd
+warnings.filterwarnings('ignore')
+
 
 def basic_preprocess(root_dir, total_swans_num, description_df):
-    images_path = os.path.join(root_dir, '\Merged\images')
-    masks_path = os.path.join(root_dir, '\Merged\masks')
-    folder_names = os.listdir(root_dir)
-    folder_names.sort()
+    # images_path = os.path.join(root_dir, '/Merged/images')
+    # masks_path = os.path.join(root_dir, '/Merged/masks')
+    images_path = root_dir + '/Merged/images/'
+    masks_path = root_dir + '/Merged/masks/'
+    print(root_dir)
+    # folder_names = os.listdir('data')
+    # folder_names.sort()
+    folder_names = ['klikun', 'maliy', 'shipun']
+    folder_names = ['data/' + x for x in folder_names]
     if not os.path.exists(images_path):
         os.makedirs(images_path)
     if not os.path.exists(masks_path):
         os.makedirs(masks_path)
     for label, folder_name in enumerate(folder_names):
-        folder_path = os.path.join(root_dir, folder_name)
-        image_path = os.path.join(folder_path, 'images')
-        mask_path = os.path.join(folder_path, 'masks')
+        # folder_path = os.path.join(root_dir, folder_name)
+
+        image_path = os.path.join(folder_name, 'images')
+        mask_path = os.path.join(folder_name, 'masks')
         image_files = os.listdir(image_path)
         mask_files = os.listdir(mask_path)
         image_files.sort()
@@ -31,22 +41,28 @@ def basic_preprocess(root_dir, total_swans_num, description_df):
         for image_file, mask_file in tqdm(zip(image_files, mask_files)):
             image = Image.open(os.path.join(image_path, image_file))
             mask = Image.open(os.path.join(mask_path, mask_file))
-            if(image.size[0] != mask.size[0] and image.size[1] != mask.size[1]): #skipping mismatching data
+            # skipping mismatching data
+            if (image.size[0] != mask.size[0] and image.size[1] != mask.size[1]):
                 continue
             if image.mode == "RGBA":
                 image = image.convert("RGB")
             try:
-                image.save(os.path.join(images_path, str(total_swans_num[0])+'.png'))
-                mask.save(os.path.join(masks_path, str(total_swans_num[0])+'.png'))
-                total_swans_num[0] +=1
-                new_row = {"swan_id": label, "image_name": str(total_swans_num[0]) + ".png", "mask_name": str(total_swans_num[0]) + ".png"}
-                description_df = description_df.append(new_row, ignore_index=True)
+                image.save(os.path.join(
+                    images_path, str(total_swans_num[0])+'.png'))
+                mask.save(os.path.join(
+                    masks_path, str(total_swans_num[0])+'.png'))
+                total_swans_num[0] += 1
+                new_row = {"swan_id": label, "image_name": str(
+                    total_swans_num[0]) + ".png", "mask_name": str(total_swans_num[0]) + ".png"}
+                description_df = description_df.append(
+                    new_row, ignore_index=True)
             except:
                 print(f"Error with image {image_file} and mask {mask_file}")
                 break
     return description_df
 
+
 description = pd.DataFrame(columns=['swan_id', 'image_name', 'mask_name'])
 total_num = [0]
-description = basic_preprocess('small_dataset', total_num, description)
+description = basic_preprocess('./small_dataset', total_num, description)
 description.to_csv('small_dataset'+"/description.csv")
